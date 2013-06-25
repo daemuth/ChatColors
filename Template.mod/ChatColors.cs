c785b4443672dab9f152deba82a61afad9d641bd
levela <1204939@live.abertay.ac.uk>
2013-06-25 15:45:23 +0200

    First commit

---
+++ Template.mod/ChatColors.cs
@@ -0,0 +1,100 @@
+using System;
+
+using ScrollsModLoader.Interfaces;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Mono.Cecil;
+using System.Collections.Generic;
+using System.Linq;
+namespace Template.mod
+{
+	public class ChatColors : BaseMod
+	{
+        List<string> sellWords = new List<string> {"WTS", "Selling", "wts", "selling"};
+        List<string> buyWords = new List<string> {"WTB", "Buying", "wtb", "buying" };
+		
+        //initialize everything here, Game is loaded at this point
+		public ChatColors ()
+		{
+            string commonSellTerm = "WTS";
+            App.ArenaChat.RoomEnter("Levela");
+		}
+
+
+		public static string GetName ()
+		{
+            return "TradeChatColors";
+		}
+
+		public static int GetVersion ()
+		{
+            return 1;
+		}
+
+		//only return MethodDefinitions you obtained through the scrollsTypes object
+		//safety first! surround with try/catch and return an empty array in case it fails
+		public static MethodDefinition[] GetHooks (TypeDefinitionCollection scrollsTypes, int version)
+		{
+            try
+            {
+                return new MethodDefinition[] {
+                    scrollsTypes["ChatRooms"].Methods.GetMethod("ChatMessage", new Type[]{typeof(RoomChatMessageMessage)}),
+            	};
+            }
+            catch
+            {
+                Console.WriteLine("Fail van!");
+                return new MethodDefinition[] { };
+            }
+		}
+
+		
+		public override bool BeforeInvoke (InvocationInfo info, out object returnValue)
+		{
+            returnValue = 0; 
+
+            if (info.targetMethod.Equals("ChatMessage")) // ChatMessage (received) in ChatRooms
+            {
+                RoomChatMessageMessage rcmm = (RoomChatMessageMessage)info.arguments[0];
+
+                string strRegex = @"\d+(gold|g|\sgold|\sg|)\b";
+                RegexOptions myRegexOptions = RegexOptions.IgnoreCase;
+                Regex firstRegex = new Regex(strRegex, myRegexOptions);
+
+                foreach (string x in sellWords)
+                {
+                    if (rcmm.text.Contains(x))
+                    {
+                        rcmm.text = firstRegex.Replace(rcmm.text, "<color=#f1f425>$&</color>");
+                        rcmm.text = rcmm.text.ToString().Replace(x, "<color=#ff4343>"+x.ToString()+"</color>");
+                    }
+                }
+
+                foreach (string x in buyWords)
+                {
+                    if (rcmm.text.Contains(x))
+                    {
+                        rcmm.text = firstRegex.Replace(rcmm.text, "<color=#f1f425>$&</color>");
+                        rcmm.text = rcmm.text.ToString().Replace(x, "<color=#4eff43>" + x.ToString() + "</color>");     
+                    }
+                }
+
+  
+                
+            }
+            else
+            {
+                
+        
+            }
+
+            return false;
+		}
+
+		public override void AfterInvoke (InvocationInfo info, ref object returnValue)
+		{
+			
+		}
+	}
+}
+
