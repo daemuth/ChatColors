ef2a6b156e2b34c2da84a63e7de5364abaec4cdf
levela <1204939@live.abertay.ac.uk>
2013-07-27 21:10:10 +0200

    Updated for new API (26/07)

--- ChatColors/ChatColors.cs
+++ ChatColors/ChatColors.cs
@@ -10,17 +10,17 @@ using System.Linq;
 namespace ChatColors
 {
 
- /*  _    _           _      ___       _               
-    / __|| |_   __ _ | |_   / __| ___ | | ___  _ _  ___
-   | (__ | ' \ / _` ||  _| | (__ / _ \| |/ _ \| '_|(_-<
-    \___||_||_|\__,_| \__|  \___|\___/|_|\___/|_|  /__/                                                    
+    /*  ___  _           _      ___       _               
+       / __|| |_   __ _ | |_   / __| ___ | | ___  _ _  ___
+      | (__ | ' \ / _` ||  _| | (__ / _ \| |/ _ \| '_|(_-<
+       \___||_||_|\__,_| \__|  \___|\___/|_|\___/|_|  /__/                                                    
 
-                    mod by levela.
-             Inspired by the idea of vimes.
-               Thanks fly out to: icbat
-                 www.scrollsguide.com
+                       mod by levela.
+                Inspired by the idea of vimes.
+                  Thanks fly out to: icbat
+                    www.scrollsguide.com
                 
-   */
+      */
 
     public class ChatColors : BaseMod
 	{
